1524e8a4327e5e2ec85a505fb14dfa06072001f1
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2009-10-08 09:38:18 +0000

    vi

--- trunk/sf/2ba3/lab2/io.c
+++ trunk/sf/2ba3/lab2/io.c
@@ -17,14 +17,61 @@ int str2int(char str[],int length,int * success){ //I pass in the length, but wh
   printf("%d\n",num1);
 }
 
+int str2float(char str[],int length,int * success){ //I pass in the length, but what do I do with it?
+  float num1;
+  if(sscanf(str, "%f", &num1)){
+     *success = 1;
+  }else{
+    *success = 0;
+  }
+  printf("%f\n",num1);
+}
+
+int str2double(char str[],int length,int * success){ //I pass in the length, but what do I do with it?
+  double num1;
+  if(sscanf(str, "%lf", &num1)){
+     *success = 1;
+  }else{
+    *success = 0;
+  }
+  printf("%lf\n",num1);
+}
+
+int str2hex(char str[],int length,int * success){ //I pass in the length, but what do I do with it?
+  int num1;
+  if(sscanf(str, "%x", &num1)){
+     *success = 1;
+  }else{
+    *success = 0;
+  }
+  printf("%x\n",num1);
+}
+
 main(){
 
   char buffer[MAX_STRING];
-  fgets(buffer, MAX_STRING, stdin);
-  int strlength = strlen(buffer);
   int valid = 0;
   int value;
+
+  printf("Hex\n");
+  fgets(buffer, MAX_STRING, stdin);
+  int strlength = strlen(buffer);
+  value = str2hex(buffer, strlength, &valid);
+
+  printf("Int\n");
+  fgets(buffer, MAX_STRING, stdin);
+  strlength = strlen(buffer);
   value = str2int(buffer, strlength, &valid);
+
+  printf("Double\n");
+  fgets(buffer, MAX_STRING, stdin);
+  strlength = strlen(buffer);
+  value = str2double(buffer, strlength, &valid);
+
+  printf("Float\n");
+  fgets(buffer, MAX_STRING, stdin);
+  strlength = strlen(buffer);
+  value = str2float(buffer, strlength, &valid);
   return !(valid);
 
 }
