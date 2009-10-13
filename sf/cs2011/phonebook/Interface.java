0ce756fccd75f8d99df9e7d451e931a0367ae2bc
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2009-10-13 13:09:07 +0000

    Finished cs2011 assignment. Just need to tidy up, comment and add newEntry removeEntry and printAll.

--- sf/cs2011/phonebook/Interface.java
+++ sf/cs2011/phonebook/Interface.java
@@ -4,20 +4,25 @@ class Interface {
 static  Phonebook phonebook = new Phonebook();
   static Scanner sc = new Scanner(System.in);
     public static void main (String Args[]){
-            for(int x = 0; x < 1; x++){
+            for(int x = 0; x < 2; x++){
         phonebook.newEntry();
       }
 
       //Create a basic command interface.
-      System.out.println("\"exit\" to exit");
+      System.out.println("\"exit\" to exit"); //Write a quick comamnd summary
       System.out.printf("Phonebook: $ ");
       String input = sc.nextLine();
       String argument;
       while(!(input.equals("exit"))){
-        String[] tokens = input.split(" "); //Split input into comamnd and argument. Creats a new string each time... I'm sorry RAM.
+        String[] tokens = input.split(" "); //Split input into comamnd and argument. Creats a new string each time...
         input = tokens[0];
-        argument = tokens[1];
+        if(tokens.length > 1){
+          argument = tokens[1];
+        }else{
+          argument = null;
+        }
         execute(input, argument);
+
         System.out.printf("\nPhonebook: $ ");
         input = sc.nextLine();
       }
@@ -27,8 +32,18 @@ static  Phonebook phonebook = new Phonebook();
 
     public static void execute(String command,String argument){
       if(command.equals("findSur")){
-            System.out.printf("Hello\n");
-          }
+            System.out.printf("%s", phonebook.displayEntry(phonebook.findSur(argument)));
+      }else if (command.equals("findFor")){
+            System.out.printf("%s", phonebook.displayEntry(phonebook.findFor(argument)));
+      }else if (command.equals("findNum")){
+            int argumentInt = Integer.parseInt(argument);
+            System.out.printf("%s", phonebook.displayEntry(phonebook.findEntry(argumentInt)));
+      }else if (command.equals("findAdd")){
+            System.out.printf("%s", phonebook.displayEntry(phonebook.findEntry(argument)));
+      }
+
+
+
     }
 
 }
