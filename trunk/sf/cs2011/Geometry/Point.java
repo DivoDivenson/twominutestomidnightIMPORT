6c00f7faabc70420fac36dc85c41b6fb2bfda21f
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2009-10-27 20:55:23 +0000

    Started cs2011 assignemt. Added skeleton Geometry, Point, Surface and Polygon classes. Still have to implement Curve, LineString(?) and LinerRing.

---
+++ trunk/sf/cs2011/Geometry/Point.java
@@ -0,0 +1,28 @@
+class Point extends Geometry{
+  private double x,y;
+  private String type = point;
+
+  public Point(double x,double y){
+    this.x = x;
+    this.y = y;
+  }
+
+  public double x(){
+    return x;
+  }
+
+  public double y(){
+    return y;
+  }
+
+  public String geometryType(){
+    return type;
+  }
+
+  public String asText(){
+    return String.format("%s X: %f Y: %f", type, x, y);  
+  }
+
+  /*Leaving out isEmpty and dimension as neither make sense for a point */
+
+}
