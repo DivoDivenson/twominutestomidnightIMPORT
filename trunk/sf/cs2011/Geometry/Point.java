d06c1b212c84701481efd1c64c4f5b38c3b58a45
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2009-11-02 20:51:44 +0000

    Finished cs2011 assignemt. I hope.

--- trunk/sf/cs2011/Geometry/Point.java
+++ trunk/sf/cs2011/Geometry/Point.java
@@ -1,27 +1,27 @@
 class Point extends Geometry{
-  private double x,y;
-  private String type = point;
+   private double x,y;
+   private String type = "Point";
 
-  public Point(double x,double y){
-    this.x = x;
-    this.y = y;
-  }
+   public Point(double x,double y){
+      this.x = x;
+      this.y = y;
+   }
 
-  public double x(){
-    return x;
-  }
+   public double x(){
+      return x;
+   }
 
-  public double y(){
-    return y;
-  }
+   public double y(){
+      return y;
+   }
 
-  public String geometryType(){
-    return type;
-  }
+   public String geometryType(){
+      return type;
+   }
 
-  public String asText(){
-    return String.format("%s X: %f Y: %f", type, x, y);  
-  }
+   public String asText(){
+      return String.format("%s X: %f Y: %f", type, x, y);  
+   }
 
   /*Leaving out isEmpty and dimension as neither make sense for a point */
 
