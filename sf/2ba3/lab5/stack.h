58ccc384b70811aa22cbd778ac74e76d36f06460
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2009-11-24 20:40:35 +0000

    Finished 2ba3 rpn calc. updated cover for it

--- sf/2ba3/lab5/stack.h
+++ sf/2ba3/lab5/stack.h
@@ -5,6 +5,12 @@ struct stack{
 };
 
 void push(struct stack * this, double item);
+
 struct stack * newStack(int size);
+
 double pop(struct stack * this);
+
+int isfull(struct stack * this);
+
+void flush(struct stack * this);
 
