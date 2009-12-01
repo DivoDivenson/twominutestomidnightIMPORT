0d87d2d9a62f97849913ecefd85dbc565fdb3a95
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2009-12-01 21:37:27 +0000

    bitset.c -> mk_union infinte loop

--- sf/2ba3/lab6/lab6.c
+++ sf/2ba3/lab6/lab6.c
@@ -4,11 +4,15 @@
 
 int main(){
 
-   struct bitset *mybitset;
+   struct bitset *mybitset,* myotherbitset;
    mybitset = bitset_new(16); //Make pointer from int without cast. What?
-   bitset_add(mybitset,3);
+   myotherbitset = bitset_new(20);
+   bitset_add(mybitset,14);
    bitset_add(mybitset,1);
-   printf("%d   %d\n",mybitset->bits[0],bitset_lookup(mybitset,2));
+   printf("%d   %d\n",mybitset->bits[0],bitset_lookup(mybitset,14));
+   bitset_remove(mybitset,14);
+   printf("%d   %d\n",mybitset->bits[0],bitset_lookup(mybitset,14));
+   mk_union(mybitset, myotherbitset);
    return 0;
     
 }
