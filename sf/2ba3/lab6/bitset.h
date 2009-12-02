58987c4ab3a14e03f18c5ee3a631d95a8c87cd03
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2009-12-02 12:08:50 +0000

    vi

--- sf/2ba3/lab6/bitset.h
+++ sf/2ba3/lab6/bitset.h
@@ -31,4 +31,6 @@ struct bitset * mk_union(struct bitset * one, struct bitset * two);
 /*place the intersection of src1 and src2 into dest*/
 //void bitset_intersect(struct bitset * dest, struct bitset * src1,struct bitset * src2);
 
+/*Returns a string representation of the set*/
+char * print_set(struct bitset * this);
 
