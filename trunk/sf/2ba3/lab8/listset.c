394a4365257171d32ce764c1cab28ebc09be17a9
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2009-12-13 19:27:56 +0000

    Linked list done (But kinda in reverse). Need to comment,clean and write test code

---
+++ trunk/sf/2ba3/lab8/listset.c
@@ -0,0 +1,101 @@
+#include "listset.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+struct listset * listset_new(){
+   struct listset * result;
+   result = malloc(sizeof(struct listelement));
+   result->head = NULL;
+   
+   return result;
+   
+}
+
+void listset_add(struct listset *this, int item){
+   
+   struct listelement * new;
+   new = malloc(sizeof(struct listelement));
+   new->value = item;
+   new->next = this->head;
+   this->current = new;
+   this->head = this->current;
+}
+
+void printlist(struct listset * this){
+   
+   struct listelement * temp;
+   while((temp = this->current)){
+      printf("%d\n",temp->value);
+      this->current = temp->next;
+   }
+   this->current = this->head;  /*Reset the listset. Point current 
+                                  back to the "top"(head)*/
+}
+
+int listset_lookup(struct listset * this,int item){
+   struct listelement * temp;
+   int result;
+   while((temp = this->current)){
+      if(temp->value == item){
+         result = 1;
+      }
+      this->current = temp->next;
+   }
+   this->current = this->head;  //Reset the list set
+   return result;
+}
+
+void listset_remove(struct listset * this, int item){ 
+   struct listelement * prev = this->head;
+   while(this->current){
+      if(this->current->value == item){
+         prev->next = this->current->next; //Unlink
+         /* The following if is true if the item to be 
+            removed is the head */
+         if(this->current == this->head){
+            this->head = this->current->next;
+         }
+         free(this->current);  //Free 
+      }
+      prev = this->current;
+      this->current = this->current->next;
+   }  
+   
+   this->current = this->head; //Reset
+}
+
+struct listset * set_union(struct listset * one, struct listset * two){
+   struct listset * result = listset_new();
+   while(one->current){
+     listset_add(result,one->current->value); 
+     one->current = one->current->next;
+   }
+   while(two->current){
+      /* If statement would prevent duplicate elements from
+         the different sets from being inserted into the result*/
+      //if(!listset_lookup(result,two->current->value)){
+      listset_add(result,two->current->value);
+      two->current = two->current->next;
+      //}
+   }
+   one->current = one->head;  //Reset the sets
+   two->current = two->head;
+   return result;
+}
+
+struct listset * set_intersect(struct listset * one, struct listset * two){
+  struct listset * result = listset_new();
+  struct listset * n;
+  n = one->current; 
+  while(one->current && two->current){ 
+      if(listset_lookup(one,two->current->value)){
+         listset_add(result,two->current->value);
+      }
+      two->current = two->current->next;
+      one->current = one->current->next;
+  }
+  one->current = one->head; //Reset the sets
+  two->current = two->head;
+  return result;
+}
+
---
+++ trunk/sf/2ba3/lab8/main.c
@@ -0,0 +1,24 @@
+#include "listset.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Almost done. Get rid of temp ballox in listset.c and write the test code */
+
+int main(){
+   struct listset * new;
+   new = listset_new();
+   listset_add(new, 12);
+   listset_add(new,13);
+   listset_add(new,12);
+   listset_add(new,77);
+   printlist(new);
+   listset_remove(new,77);
+   //listset_remove(new,13);
+   printf("New\n");
+   printlist(new);
+   struct listset * two = listset_new();
+   listset_add(two,12);
+   printf("Union\n");
+   printlist(set_intersect(new,two));
+   return 0;
+}
