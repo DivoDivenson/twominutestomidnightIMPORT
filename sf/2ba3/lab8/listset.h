59c5a842b3a9cacecba50d5f0fa3b5cdbc33a3d0
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2009-12-13 19:27:56 +0000

    Linked list done (But kinda in reverse). Need to comment,clean and write test code

--- sf/2ba3/lab8/listset.h
+++ sf/2ba3/lab8/listset.h
@@ -1,16 +1,32 @@
+/* Appaneds stuff to start of the list. FIFO structure. Is this win/fail? */
+
 struct listset{
-   struct listelement * nextelement;
-}
+   struct listelement * current;
+   struct listelement * head;
+};
 
 struct listelement{
    int value;
-   struct listelement * nextelement;
-}
+   struct listelement * next;
+};
 
-struct listset * listset_new(){
-   struct listset * result;
-   result = malloc(sizeof(struct listset));
 
-   return result;
-   
-}
+struct listset * listset_new();
+
+/* As above, inserts new element to top of list */
+void listset_add(struct listset * this, int item);
+
+void printlist(struct listset * this);
+
+/* 1 if there, 0 if not*/
+int listset_lookup(struct listset * this,int item);
+
+/* Removes all instances of item from the list */
+void listset_remove(struct listset * this, int item);
+
+/* return the union of the two argument as a new set */
+struct listset * set_union(struct listset * one, struct listset * two);
+
+/* Return the intersection of the twp arguments as a new set */
+struct listset * set_intersect(struct listset * one, struct listset * two);
+
