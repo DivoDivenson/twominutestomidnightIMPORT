6917120f11cb19cf2047f7d6a98583f2b18280ac
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-02-03 16:08:24 +0000

    vim

---
+++ sf/CS2031/Assignment1/bitset.h
@@ -0,0 +1,39 @@
+struct bitset{
+  unsigned char * bits;
+  int size;
+  int bit_size;
+};
+
+
+
+/*create a new, empty bit vector set of 'size' items*/
+struct bitset *bitset_new(int size);
+
+/*check to see if an item is in the set
+returns 1 if in the set, 0 if not, and -1 if 'item' is out of bounds*/
+int bitset_lookup(struct bitset * this, int item);
+/*Print the entire bitset. Mainly for debugging*/
+//char print_set(struct bitset * this);
+
+/*add an item, with number 'item' to the set
+(returns 0 if item is out of bounds, 1 otherwise)
+has no effect if the item is already in the set*/
+//int bitset_add(struct bitset * this, int item);
+int bitset_add(struct bitset * this,int item);
+
+/*remove an item with number 'item' from the set
+(returns 0 if item is out of bounds, 1 otherwise)*/
+int bitset_remove(struct bitset * this, int item);
+
+/*Return the union of the two input sets*/
+struct bitset * mk_union(struct bitset * one, struct bitset * two);
+
+/*place the intersection of src1 and src2 into dest*/
+//void bitset_intersect(struct bitset * dest, struct bitset * src1,struct bitset * src2);
+struct bitset * intersect(struct bitset * one, struct bitset * two);
+
+/*Returns a string representation of the set*/
+char * print_set(struct bitset * this);
+
+/*Return 1 if the parity of the bitset is 1, 0 if odd */
+int isEven(struct bitset * this);
