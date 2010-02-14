1db54e92851fee0e0718de8fc51188d531fb164d
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-02-14 21:47:20 +0000

    Infoman Done: add delete delete's search print Yet to do: updatefield compress sort changeDelim

--- trunk/sf/CS2041/Serial/Main.java
+++ trunk/sf/CS2041/Serial/Main.java
@@ -3,7 +3,11 @@ class Main{
    public static void main(String args[]){
 
      SerialFile file = new SerialFile("A01.txt"); 
-     file.printRecord(2);
+     System.out.println(file.printRecord(8832321));
+     System.out.println(file.printRecord(123448));
+     System.out.println(file.searchRecord("LN","Duggan"));
+     file.deleteRecord(431223);
+     file.addRecord(123444,"Hender","Mark","Scrub",false);
    }
 
    
