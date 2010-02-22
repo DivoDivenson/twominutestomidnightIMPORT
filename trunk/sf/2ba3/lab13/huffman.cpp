99fd1214d3bb544a6cc1b31e1dd2b0e626d8828e
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-02-22 13:50:23 +0000

    Started lab13

---
+++ trunk/sf/2ba3/lab13/huffman.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+class node{
+  private:
+    int freq;
+}
+
+class huffman{
+  private:
+
+  public:
+    huffman(std::string file);
+
+
+
+
+
+}
+
+
+
+int read_in(std::string file){
+    std::cout << file;
+}
+
+int main(int args, char ** argv){
+  int 
+  read_in(argv[1]);
+
+}
