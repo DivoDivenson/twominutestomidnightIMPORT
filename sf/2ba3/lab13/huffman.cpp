2a3431cda1958f49093c24f448206ebbb41d4536
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-02-22 22:35:21 +0000

    Chars read in and freq calculated

--- sf/2ba3/lab13/huffman.cpp
+++ sf/2ba3/lab13/huffman.cpp
@@ -4,30 +4,52 @@
 
 
 class node{
-  private:
+  public:  //Simple containers. Use public instead of setter?
     int freq;
-}
+};
 
-class huffman{
+class compound_node : public node{
+   public:
+      node * left;
+      node * right;
+};
+
+class simple_node : public node{
+   public:
+      char c;
+};
+
+class Huffman{
   private:
-
+     int size;
+     node ** table;
   public:
-    huffman(std::string file);
+    Huffman();
+    //open_file(); To hell with it, open it in constructor
 
+};
 
-
-
-
+Huffman::Huffman(){
+   size = 256; //Full ascii character set
+   table = new node*[size];
+   for(int i = 0; i < size; i++){
+      table[i] = new node;
+      table[i]->freq = 1;
+   }
+   std::ifstream file;
+   file.open("input.txt");
+   char c;
+   while((c = file.get()) != -1){
+      //std::cout <<  c << " ";
+      table[(int) c]->freq++;
+   }
+   file.close();
 }
 
 
 
-int read_in(std::string file){
-    std::cout << file;
-}
 
 int main(int args, char ** argv){
-  int 
-  read_in(argv[1]);
+   Huffman * huffman = new Huffman();
 
 }
