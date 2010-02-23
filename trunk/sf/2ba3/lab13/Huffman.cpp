25ffba74f0f65306c705274154b2e204c856cc2b
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-02-23 22:06:31 +0000

    Too much trouble with inheritance

--- trunk/sf/2ba3/lab13/Huffman.cpp
+++ trunk/sf/2ba3/lab13/Huffman.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <string>
 
+using std::cout;
 
 class node{
   public:  //Simple containers. Use public instead of setter?
     int freq;
+    virtual ~node() {}
 };
 
 class compound_node : public node{
@@ -23,27 +25,95 @@ class Huffman{
   private:
      int size;
      node ** table;
+     node * merge(node * node1, node * node2);
+     void genTree();
+     void sortTable(int left, int right);
   public:
     Huffman();
+    void print(); //Debug method
     //open_file(); To hell with it, open it in constructor
 
 };
+
+void Huffman::print(){
+   int x = 0;
+   for(int i = 0; i < size; i++){
+      cout << table[i]->freq;
+      x += table[i]->freq;
+   }
+   cout << "\n" << x << "\n";
+
+}
 
 Huffman::Huffman(){
    size = 256; //Full ascii character set
    table = new node*[size];
    for(int i = 0; i < size; i++){
       table[i] = new node;
+      //simple_node * tempNode = static_cast<simple_node*>(table[i]);
+      table[i] = dynamic_cast<simple_node*>(table[i]);
+      //table[i] = tempNode;
       table[i]->freq = 1;
+      table[i]->c = 'i';
    }
    std::ifstream file;
    file.open("input.txt");
    char c;
    while((c = file.get()) != -1){
-      //std::cout <<  c << " ";
+      //cout <<  c << " ";
       table[(int) c]->freq++;
    }
    file.close();
+   print();
+   //Generate the tree here
+   genTree();
+}
+
+void Huffman::genTree(){
+   size -= 1; //Compensate for array
+   while(table[1] != NULL){
+      sortTable(0, size);
+      table[0] = merge(table[0], table[1]);
+      table[1] = table[size];
+      table[size--] = NULL;
+   }
+}
+//node1 <= node2
+node * Huffman::merge(node * node1, node * node2){
+   compound_node * result = new compound_node();
+   result->left = node1;
+   result->right = node2;
+   result->freq = node1->freq + node2->freq;
+
+   return result;
+}
+
+
+//Quick sort the array. Should probably pass the array as a param
+void Huffman::sortTable(int left, int right){
+   int i = left, j = right;
+   node * temp;
+   int pivot = table[(left + right) / 2]->freq;
+
+   while(i <= j){
+      while(table[i]->freq < pivot)
+         i++;
+      while (table[j]->freq > pivot)
+         j--;
+      if( i <= j ){
+         temp = table[i];
+         table[i] = table[j];
+         table[j] = temp;
+         i++;
+         j--;
+      }
+   }
+
+   if(left < j)
+      sortTable(left,j);
+   if(i < right)
+      sortTable(i, right);
+
 }
 
 
@@ -51,5 +121,8 @@ Huffman::Huffman(){
 
 int main(int args, char ** argv){
    Huffman * huffman = new Huffman();
+   //huffman->sortTable(0,255);
+   //huffman->print();
+   //huffman->genTree();
 
 }
