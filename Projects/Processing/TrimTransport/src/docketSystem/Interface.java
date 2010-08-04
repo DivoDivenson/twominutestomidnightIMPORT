1cbbe9328f2b5a544f47e4e9598b0d9a7eb61481
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-08-04 16:28:50 +0000

    Added books.sql

--- Projects/Processing/TrimTransport/src/docketSystem/Interface.java
+++ Projects/Processing/TrimTransport/src/docketSystem/Interface.java
@@ -1,6 +1,7 @@
 package docketSystem;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -12,133 +13,239 @@ import org.jopendocument.print.DefaultDocumentPrinter;
 
 public class Interface extends JFrame {
 
-	JPanel main;
+	private JPanel main;
 
-	JMenuBar menu;
-	JMenu file;
+	private JMenuBar menu;
 
-	JButton newDoc;
+	private JButton newDoc;
 
-	Invoice invoice;
-	NewDocForm newDocForm;
-	
-	Document document;
+	private Invoice invoice;
+	private NewDocForm newDocForm;
+
+	private Document document;
+	private JComponent stuff[];
+
+
+	private javax.swing.JButton closeButton;
+	private javax.swing.JMenu file;
+	private javax.swing.JMenu edit;
+	private javax.swing.JMenuBar jMenuBar1;
+
+	private javax.swing.JButton searchButton;
 
 	public Interface() {
 		initComponents();
 	}
 
 	private void initComponents() {
+
+		
+		main = new javax.swing.JPanel();
+		newDoc = new javax.swing.JButton();
+		searchButton = new javax.swing.JButton();
+		closeButton = new javax.swing.JButton();
+		jMenuBar1 = new javax.swing.JMenuBar();
+		file = new javax.swing.JMenu();
+		edit = new javax.swing.JMenu();
+
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-		main = new JPanel();
-		initMenu();
-		initPanel();
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
-				getContentPane());
-		getContentPane().setLayout(layout);
 
-		layout.setHorizontalGroup(layout.createParallelGroup(
-				javax.swing.GroupLayout.Alignment.LEADING).addComponent(main,
-				javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE));
-		layout.setVerticalGroup(layout.createParallelGroup(
-				javax.swing.GroupLayout.Alignment.LEADING).addComponent(main,
-				javax.swing.GroupLayout.DEFAULT_SIZE, 281, Short.MAX_VALUE));
-
-		java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit()
-				.getScreenSize();
-		setBounds((screenSize.width - 410) / 2, (screenSize.height - 330) / 2,
-				410, 330);
-
-	}
-	
-	public void saveActionPreformed(){
-		;
-	}
-	
-	public void printActionPreformed(Invoice invoice){
-		document = new Document(invoice);
-		final OpenDocument doc = new OpenDocument();
-		doc.loadFrom("src/data/invoice1.ods");
-		
-
-		// Show time !
-		final JFrame mainFrame = new JFrame("Viewer");
-		DefaultDocumentPrinter printer = new DefaultDocumentPrinter();
-		
-
-		ODSViewerPanel viewerPanel = new ODSViewerPanel(doc, printer, true);
-		
-
-		java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit()
-		.getScreenSize();
-		mainFrame.setContentPane(viewerPanel);
-		mainFrame.pack();
-		mainFrame.setLocation(screenSize.width /2, (screenSize.height)/2);
-		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		mainFrame.setVisible(true);
-	}
-
-	private void initPanel() {
-		newDoc = new JButton("new");
+		newDoc.setText("New");
 		newDoc.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
 				newDocActionPerformed(evt);
 			}
 		});
 
-		// I'm using netbeans to create GUI code, so it's gonna be shit....
+		searchButton.setText("Find");
+		searchButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				searchButttonActionPerformed();
+			}
+		});
+
+		closeButton.setText("Close");
+		closeButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				closeButtonActionPerformed();
+			}
+		});
+
 		javax.swing.GroupLayout mainLayout = new javax.swing.GroupLayout(main);
 		main.setLayout(mainLayout);
-		mainLayout.setHorizontalGroup(mainLayout.createParallelGroup(
-				javax.swing.GroupLayout.Alignment.LEADING).addGroup(
-				mainLayout.createSequentialGroup().addContainerGap()
-						.addComponent(newDoc,
-								javax.swing.GroupLayout.PREFERRED_SIZE, 66,
-								javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addContainerGap(322, Short.MAX_VALUE)));
-		mainLayout.setVerticalGroup(mainLayout.createParallelGroup(
-				javax.swing.GroupLayout.Alignment.LEADING).addGroup(
-				mainLayout.createSequentialGroup().addContainerGap()
-						.addComponent(newDoc,
-								javax.swing.GroupLayout.PREFERRED_SIZE, 68,
-								javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addContainerGap(201, Short.MAX_VALUE)));
+		mainLayout
+				.setHorizontalGroup(mainLayout
+						.createParallelGroup(
+								javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(
+								mainLayout
+										.createSequentialGroup()
+										.addContainerGap()
+										.addGroup(
+												mainLayout
+														.createParallelGroup(
+																javax.swing.GroupLayout.Alignment.LEADING)
+														.addComponent(
+																newDoc,
+																javax.swing.GroupLayout.PREFERRED_SIZE,
+																66,
+																javax.swing.GroupLayout.PREFERRED_SIZE)
+														.addComponent(
+																searchButton,
+																javax.swing.GroupLayout.PREFERRED_SIZE,
+																66,
+																javax.swing.GroupLayout.PREFERRED_SIZE)
+														.addComponent(
+																closeButton,
+																javax.swing.GroupLayout.PREFERRED_SIZE,
+																66,
+																javax.swing.GroupLayout.PREFERRED_SIZE))
+										.addContainerGap(426, Short.MAX_VALUE)));
+		mainLayout
+				.setVerticalGroup(mainLayout
+						.createParallelGroup(
+								javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(
+								mainLayout
+										.createSequentialGroup()
+										.addContainerGap()
+										.addComponent(
+												newDoc,
+												javax.swing.GroupLayout.PREFERRED_SIZE,
+												68,
+												javax.swing.GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(
+												javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+										.addComponent(
+												searchButton,
+												javax.swing.GroupLayout.PREFERRED_SIZE,
+												68,
+												javax.swing.GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(
+												javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+										.addComponent(
+												closeButton,
+												javax.swing.GroupLayout.PREFERRED_SIZE,
+												68,
+												javax.swing.GroupLayout.PREFERRED_SIZE)
+										.addContainerGap(33, Short.MAX_VALUE)));
 
+		file.setText("File");
+		jMenuBar1.add(file);
+
+		edit.setText("Edit");
+		jMenuBar1.add(edit);
+
+		setJMenuBar(jMenuBar1);
+
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
+				getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(layout.createParallelGroup(
+				javax.swing.GroupLayout.Alignment.LEADING).addComponent(main,
+				javax.swing.GroupLayout.DEFAULT_SIZE,
+				javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+		layout.setVerticalGroup(layout.createParallelGroup(
+				javax.swing.GroupLayout.Alignment.LEADING).addComponent(main,
+				javax.swing.GroupLayout.DEFAULT_SIZE,
+				javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+
+		java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit()
+				.getScreenSize();
+		setBounds((screenSize.width - 514) / 2, (screenSize.height - 322) / 2,
+				514, 322);
+		setFonts();
 	}
+
+	public void saveActionPreformed() {
+		;
+	}
+	
+	public void setFonts(){
+		stuff = new JComponent[]{newDoc, searchButton, closeButton, file, edit};
+		for(JComponent n : stuff){
+			n.setFont(new java.awt.Font("Dialog", 0, 11));
+		}
+	}
+
+	public void printActionPreformed(Invoice invoice) {
+		document = new Document(invoice);
+		final OpenDocument doc = new OpenDocument();
+		doc.loadFrom("src/data/invoice1.ods");
+
+		// Show time !
+		final JFrame mainFrame = new JFrame("Viewer");
+		DefaultDocumentPrinter printer = new DefaultDocumentPrinter();
+
+		ODSViewerPanel viewerPanel = new ODSViewerPanel(doc, printer, true);
+
+		java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit()
+				.getScreenSize();
+		mainFrame.setContentPane(viewerPanel);
+		mainFrame.pack();
+		mainFrame.setLocation(screenSize.width / 2, (screenSize.height) / 2);
+		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		mainFrame.setVisible(true);
+	}
+
+	/*
+	 * private void initPanel() { newDoc = new JButton("new");
+	 * newDoc.addActionListener(new java.awt.event.ActionListener() { public
+	 * void actionPerformed(java.awt.event.ActionEvent evt) {
+	 * newDocActionPerformed(evt); } });
+	 * 
+	 * // I'm using netbeans to create GUI code, so it's gonna be shit....
+	 * javax.swing.GroupLayout mainLayout = new javax.swing.GroupLayout(main);
+	 * main.setLayout(mainLayout);
+	 * mainLayout.setHorizontalGroup(mainLayout.createParallelGroup(
+	 * javax.swing.GroupLayout.Alignment.LEADING).addGroup(
+	 * mainLayout.createSequentialGroup().addContainerGap()
+	 * .addComponent(newDoc, javax.swing.GroupLayout.PREFERRED_SIZE, 66,
+	 * javax.swing.GroupLayout.PREFERRED_SIZE) .addContainerGap(322,
+	 * Short.MAX_VALUE)));
+	 * mainLayout.setVerticalGroup(mainLayout.createParallelGroup(
+	 * javax.swing.GroupLayout.Alignment.LEADING).addGroup(
+	 * mainLayout.createSequentialGroup().addContainerGap()
+	 * .addComponent(newDoc, javax.swing.GroupLayout.PREFERRED_SIZE, 68,
+	 * javax.swing.GroupLayout.PREFERRED_SIZE) .addContainerGap(201,
+	 * Short.MAX_VALUE)));
+	 * 
+	 * }
+	 */
 
 	private void newDocActionPerformed(java.awt.event.ActionEvent evt) {
-		
-		//SET THE DOC NUMBER IF IT IS USED
-		//newInvoice = new Invoice();
+
+		// SET THE DOC NUMBER IF IT IS USED
+		// newInvoice = new Invoice();
 		if (newDocForm != null)
-			newDocForm = null;	//Just in case
+			newDocForm = null; // Just in case
 		newDocForm = new NewDocForm(this);
 		newDocForm.setVisible(true);
-		
 
 	}
 	
-	public void setInvoice(Invoice invoice){
-		this.invoice = invoice;
-		saveActionPreformed();
-		//printActionPreformed(this.invoice);
+	private void searchButttonActionPerformed(){
+		;
 	}
 	
-	
-	
-	
-	
-	
-	public Invoice getInvoice(){
+	private void closeButtonActionPerformed(){
+		this.setVisible(false);
+		this.dispose();
+	}
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+		saveActionPreformed();
+		// printActionPreformed(this.invoice);
+	}
+
+	public Invoice getInvoice() {
 		return invoice;
 	}
 
-	private void initMenu() {
-		menu = new JMenuBar();
-		file = new JMenu();
-		file.setText("File");
-		menu.add(file);
-		setJMenuBar(menu);
-	}
+	/*
+	 * private void initMenu() { menu = new JMenuBar(); file = new JMenu();
+	 * file.setText("File"); menu.add(file); setJMenuBar(menu); }
+	 */
 
 }
