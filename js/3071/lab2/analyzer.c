3828564a5b37f135dc4e73e41f4dac603603bdf1
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-10-16 19:37:27 +0000

    Compiler design lab2 done.

--- js/3071/lab2/analyzer.c
+++ js/3071/lab2/analyzer.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int start(char *s){
+int digit(char *s){
 
    int i, result;
    result = 0;
@@ -12,7 +12,7 @@ int start(char *s){
    return result;
 }
 
-int check(char *s){
+int check(char *s, int sign){
    int i;
    for(i = 0; isdigit(s[i]); i++)
       ;
@@ -20,20 +20,43 @@ int check(char *s){
       return 1;
    }else if(i > 5){
       return 0;
-   }
+   }else{
    //else do some sort of character compare  
+   //this is gonna be a bit brutal....
+      if(s[0] >= '3')
+         if(s[1] >= '2')
+            if(s[2] >= '7')
+                  if(s[3] >= '6')
+                     if(sign == 1 && s[4] > '7'){
+                        //Overflow
+                        return 0;
+                     }else if(s[4] > '8'){ //Sign will have to be 1 or -1 by the time this code executes
+                        return 0;
+                     }
+
+      return 1;
+   }
 }
+
 
 int main(int argc, char * argv[]){
 
    char *c;
    int val = 0;
-   int sign;
+   int sign = 1;
 
    c = argv[1];
 
-   if(check(c))
-      val = start(c);
+
+   if(c[0] == '-'){
+      sign = -1;
+      c = &c[1];
+   }else if(c[0] == '+'){
+      c = &c[1];
+   }
+
+   if(check(c, sign))
+      val = digit(c) * sign;
 
    printf("%d\n",val);
 
