3cb0a97ba86041d7e34331e8f5b699d3640fe4e3
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-10-18 09:53:06 +0000

    3071 lab3 started

---
+++ js/3071/lab3/ident.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[]){
+
+  char * string;
+  char * buffer;
+
+  string = argv[1];
+  buffer = malloc(sizeof(char) * 8);
+
+
+
+  return 0;
+
+}
+
+
