e17fe300afcfb19ea33d1eff58755e5dc5414130
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-10-26 14:52:00 +0000

    Prolog q1 done...

--- js/3011/assign1/kb1.pl
+++ js/3011/assign1/kb1.pl
@@ -5,9 +5,18 @@ numeral(X + Y) :- numeral(X), numeral(Y).
 add(0,X,X).
 add(s(X), Y, s(Z)) :- add(X,Y,Z).
 
+add2(0,Y,Z) :- eval(Y,Z).
+add2(s(W),Y,s(Z)):-
+	add2(W,Y,Z).
+add2(X,0,Z) :- eval(X,Z).
+add2(W,s(Y),s(Z)):-	
+	add2(W,Y,Z).
+add2(U+V,Y,Z):-
+	add2(U,V,W),
+	add2(W,Y,Z).
 
+eval(0,0).
+eval(s(X),s(X)).
+eval(X+Y,Z):-
+	add2(X,Y,Z).
 
-reduce(numeral(X)):- numeral(X).
-reduce(numeral(X + Y), Z):- add(X,Y,Z).
-
-add2(X,Y,Z):- reduce(X,Q), reduce(Y,R), add(Q,R,Z).
