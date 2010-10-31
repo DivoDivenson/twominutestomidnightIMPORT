7756fa081c134425b302a01c8bdc8822bb998f16
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-10-31 20:44:58 +0000

    FUCK PROLOG

--- js/3011/assign1/secondTry.pl
+++ js/3011/assign1/secondTry.pl
@@ -7,11 +7,19 @@ add(0,X,X).
 add(s(X),Y,s(Z)) :- add(X,Y,Z).
 
 add2(0,Y,Z) :- eval(Y,Z).
+%add2(X,0,Z) :- eval(X,Z).
 add2(s(W),Y,s(Z)) :- add2(W,Y,Z).
 add2(U+V,Y,Z) :-
    add2(U,V,W),
    add2(W,Y,Z).
 
+add2(p(W),Y,Z):-add2(W,Y,s(Z)).
+add2(p(W),Y,p(Z)):-add2(W,Y,Z).
+
 eval(0,0).
 eval(s(X),s(Z)) :- eval(X,Z).
-eval(X+Y,Z):-add2(X,Y,Z).
+%eval(p(X),p(Z)) :- eval(X,Z).
+eval(s(p(X)),Z) :- eval(X,Z).
+eval(p(s(X)),Z) :- eval(X,Z).
+eval(X+Y,Z) :- add2(X,Y,Z).
+
