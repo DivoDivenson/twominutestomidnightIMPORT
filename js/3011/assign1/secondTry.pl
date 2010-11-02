f9f31a2f53cdd5c66e316fa85e9d9d1ce3facd42
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-11-02 13:22:37 +0000

    Prolog done

--- js/3011/assign1/secondTry.pl
+++ js/3011/assign1/secondTry.pl
@@ -17,26 +17,26 @@ add2(U-V,Y,Z) :-
   subtract(U,V,W),
   add2(W,Y,Z).
 
-add2(p(W),Y,Z):-add2(W,Y,s(Z)).
-add2(p(W),Y,p(Z)):-add2(W,Y,Z).
+add2(p(W),Y,Z):-add2(W,Y,s(Z)),!.
+add2(p(W),Y,p(Z)):-add2(W,Y,Z),!.
 
-add2(-X,Y,Z) :- minus(X,W), add2(W,Y,Z).
-add2(X,-Y,Z) :- minus(Y,W), add2(X,W,Z).
+add2(-X,Y,Z) :- minus(X,W), add2(W,Y,Z),!.
+add2(X,-Y,Z) :- minus(Y,W), add2(X,W,Z),!.
 
 eval(0,0).
-eval(p(X),p(Z)) :- eval(X,Z).
-eval(s(X),s(Z)) :- eval(X,Z).
-eval(s(p(X)),Z) :- eval(X,Z).
-eval(p(s(X)),Z) :- eval(X,Z).
-eval(X+Y,Z) :- add2(X,Y,W), eval(W,Z).
+eval(p(X),p(Z)) :- eval(X,Z),!.
+eval(s(X),s(Z)) :- eval(X,Z),!.
+eval(s(p(X)),Z) :- eval(X,Z),!.
+eval(p(s(X)),Z) :- eval(X,Z),!.
+eval(X+Y,Z) :- add2(X,Y,W), eval(W,Z),!.
 
 minus(0,0).
-minus(s(p(X)),Z) :- minus(X,Z).
-minus(p(s(X)),Z) :- minus(X,Z).
-minus(s(X),p(Z)) :- minus(X,Z).
-minus(p(X),s(Z)) :- minus(X,Z).
-minus(X-Y,Z) :- subtract(X,Y,W), minus(W,Z).
+minus(s(p(X)),Z) :- minus(X,Z),!.
+minus(p(s(X)),Z) :- minus(X,Z),!.
+minus(s(X),p(Z)) :- minus(X,Z),!.
+minus(p(X),s(Z)) :- minus(X,Z),!.
+minus(X-Y,Z) :- subtract(X,Y,W), minus(W,Z),!.
 
-subtract(X,Y,Z) :- minus(Y,W), add2(X,W,Z).
-subtract(X,-Y,Z) :- add2(X,Y,Z).
-subtract(-X,Y,Z) :- add2(X,Y,Z).
+subtract(X,Y,Z) :- minus(Y,W), add2(X,W,Z),!.
+subtract(X,-Y,Z) :- add2(X,Y,Z),!.
+subtract(-X,Y,Z) :- add2(X,Y,Z),!.
