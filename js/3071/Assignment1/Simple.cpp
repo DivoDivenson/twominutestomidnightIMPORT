803e9be30bc53097742154b8579299954184504d
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-11-10 20:14:08 +0000

    Compiler design almost done

--- js/3071/Assignment1/Simple.cpp
+++ js/3071/Assignment1/Simple.cpp
@@ -1,13 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 #include "Parser.h"
 #include "Scanner.h"
-#include <wchar.h>
+#include<wchar.h>
 
 int main(int argc, char *argv[]){
-   wchar_t *fileName = coco_string_create(argv[1]);
+   wchar_t *fileName = coco_string_create(argv[1]); //Don't even need this but what the hell.
    Scanner *scanner = new Scanner(fileName);
    Parser *parser = new Parser(scanner);
    parser->tab = new map<string, int>;
    parser->Parse();
 }
-
