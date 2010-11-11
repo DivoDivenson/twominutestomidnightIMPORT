b9e6a647b47d2d8f00d064d7035b3c1eb6c1d637
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-11-11 21:18:02 +0000

    Need to write an interpreter, no idea how though

--- js/3071/assign1/Compile.cpp
+++ js/3071/assign1/Compile.cpp
@@ -1,13 +1,21 @@
-#include <stdio.h>
 #include "Parser.h"
 #include "Scanner.h"
 #include <wchar.h>
+#include <iostream>
 
 int main(int argc, char *argv[]){
-   wchar_t *fileName = coco_string_create(argv[1]);
-   Scanner *scanner = new Scanner(fileName);
+   string input;
+   
+   cout << "Enter values and stuff:";
+   getline(cin, input);
+//   wchar_t *fileName = coco_string_create(argv[1]);
+   Scanner *scanner = new Scanner((unsigned char*)input.c_str(), input.length());
    Parser *parser = new Parser(scanner);
    parser->tab = new map<string, int>;
    parser->Parse();
+  /* scanner = new Scanner((unsigned char*)input.c_str(), input.length());
+   parser = new Parser(scanner);
+   parser->Parse();*/
+
 }
 
