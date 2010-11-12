ecbd853f5e40e8c71d15d04993fee4f23326940f
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2010-11-12 12:20:47 +0000

    almost

--- trunk/js/3071/assign1/Compile.cpp
+++ trunk/js/3071/assign1/Compile.cpp
@@ -2,19 +2,34 @@
 #include "Scanner.h"
 #include <wchar.h>
 #include <iostream>
+#include <map>
+#include <string>
 
 int main(int argc, char *argv[]){
    
    cout << "Enter values and stuff:\n";
-   wchar_t *fileName = coco_string_create(argv[1]);
+   /*wchar_t *fileName = coco_string_create(argv[1]);
    Scanner *scanner = new Scanner(fileName);
    //Scanner *scanner = new Scanner(stdin);
    Parser *parser = new Parser(scanner);
    parser->tab = new map<string, int>;
-   parser->Parse();
+   int i;
+
   /* scanner = new Scanner((unsigned char*)input.c_str(), input.length());
    parser = new Parser(scanner);
    parser->Parse();*/
+
+   char buffer[100];
+   //string buffer; Was passing buffer.c_str() to the scanner but it was not working. No '\0', doing it the "C" way works fine.
+   map<string, int> * store = new map<string, int>;
+   while(fgets(buffer, 100, stdin) != NULL){
+      Scanner * scanner = new Scanner((unsigned char *) buffer, strlen(buf)); 
+      Parser * parser = new Parser(scanner);
+      parser->tab = store;
+      parser->Parse();
+      delete parser;
+      delete scanner;
+   }
 
 }
 
