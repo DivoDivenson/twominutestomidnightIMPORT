456a60791b7e79c14dea7ac927ee4630b80f6251
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-01-19 14:48:37 +0000

    Prolog exercise 5 done

---
+++ trunk/js/3011/ex5.pl
@@ -0,0 +1,28 @@
+increment(X,Y):- X is Y -1.
+
+sum(X,Y,Z):- Z is Y + X.
+
+
+addone([],[]).
+addone([X|Y],[R|S]):- R is X +1, addone(Y,S).
+
+accMin([H|T],A,Min):-
+  H < A,
+  accMin(T,H,Min).
+
+accMin([H|T],A,Min):-
+  H >= A,
+  accMin(T,A,Min).
+
+accMin([],A,A).
+
+min(List,Min):-
+  List = [H|_],
+  accMin(List,H,Min).
+
+
+scalerMult(_,[],[]).
+scalerMult(A,[H|T],[X|Y]):-
+  X is H * A,
+  scalerMult(A,T,Y).
+
