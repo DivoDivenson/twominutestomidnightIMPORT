dae62054e83c719b49de40a3c53be5e8fea7ccdd
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-01-26 14:22:47 +0000

    some more prolog done

--- js/3011/ex6.pl
+++ js/3011/ex6.pl
@@ -1,5 +1,7 @@
 append([],L,L).
 append([H|T],L2,[H|L3]):-append(T,L2,L3).
+                          
+
 
 double(X):-append(Y,Y,X).
 
@@ -8,3 +10,17 @@ accRev([H|T],A,R):- accRev(T,[H|A],R).
 accRev([],A,A).
 
 palindrome(X):- accRev(X,[],X).
+
+second(X,[_|T]):- head(X,T).
+head(X,[H|_]):- H = X.
+
+swap12([],[]).
+swap12([H|T],[A|B]):- head(H,B), head(A,T), pad(T,B).
+pad([H|T],[A|B]):- equal(T,B).
+equal([],[]).
+equal([H|T],[A|B]) :- H = A, equal(T,B).
+
+
+
+final(X,[H|T]):- H = X, T = [].
+final(X,[H|T]):- final(X,T).
