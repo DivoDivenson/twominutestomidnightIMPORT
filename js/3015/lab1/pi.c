18274e842e0bfd9f732a58d2adb91d69afcdebb3
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-01-27 23:34:05 +0000

    Concurrent systems lab done i think

--- js/3015/lab1/pi.c
+++ js/3015/lab1/pi.c
@@ -4,38 +4,53 @@
 #include <math.h>
 #include <pthread.h>
 
+#define NUM_THREADS 6
+pthread_mutex_t   mutex = PTHREAD_MUTEX_INITIALIZER;
+int final_count = 0;
+int darts = 1000000;//Darts is only ever read from
+
+
+//1) Divide up iterations. Could randomize number so threads don't all finish at once and go for the lock
+//2) Run method once per thread, sum the counts,then divide by NUM_THREADS. Wait. That's stupid
 //pass in darts as param
 void *getPi(void *argument){
-  int darts = 500000;
+   printf("Arg :%d\n\n", (int)argument); 
+   
   double x,y,hyp;
   srand(time(NULL));
   int i, count = 0;
-  for(i = 0; i < darts; i++){
+  int loop_amount = darts / NUM_THREADS; //Divide up the work
+  for(i = 0; i < loop_amount; i++){
     x = rand()/(RAND_MAX + 1.0);
     y = rand()/(RAND_MAX + 1.0);
     hyp = (x * x) + (y * y);
-    hyp = sqrt(hyp);
+    //hyp = sqrt(hyp); // (x < 1) == ( x^2 < 1)
     if(hyp <= 1){ 
       count++;
     }
   }
-  return (double) ((4.0 *count) / darts);
-  //return (void *)
+  pthread_mutex_lock(&mutex);
+  final_count += count;
+  pthread_mutex_unlock(&mutex);
+//  return (double) ((4.0 *count) / darts);
+
+  //return (void *)*/
 }
 
 int main(){
-
   pthread_t threads[NUM_THREADS];
-  int thread_args[NUM_THREADS];
+
   int rc, i;
   for(i=0; i<NUM_THREADS; i++){
-    thread_args[i] = i;
-    rc = pthread_create(&threads[i], NULL, getPi, (void *) &thread_args[i]);
+    printf("Creating thread\n");
+    rc = pthread_create(&threads[i], NULL, getPi, (void *)i);
   }
   for(i = 0; i < NUM_THREADS; i++){
     rc = pthread_join(threads[i], NULL);
   }
-  printf("%lf\n",getPi());
+  final_count = final_count;// / NUM_THREADS;
+  printf("Pi is : %lf\n", (double)((4.0 * final_count) / darts));
+  pthread_mutex_destroy(&mutex);
   return 0;
 
 }
