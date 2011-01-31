9782345dfe14418b97fb4890735d81adc1466f2b
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-01-31 13:21:28 +0000

    ackerman code done

---
+++ js/3021/lab1/ackermanTwo.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*
+Stock code takes ~1700 microseconds
+
+   */
+int count= 0;
+int overflow = 0;
+int underflow= 0;
+int windowSize = 6;
+int windowCount = 0;
+
+int ackerman(int x, int y){
+   count++;
+   int result;
+   if(windowCount == windowSize -1){
+      overflow++;
+   }else{
+      windowCount++;
+   }
+   if(x == 0)
+      result =  y+1;
+   else if (y ==0)
+      result =  ackerman(x-1,1);
+   else
+      result =  ackerman(x-1, ackerman(x, y -1));
+   if(windowCount <= 0){
+      underflow++;
+   }else{
+      windowCount--;
+   }
+
+   return result;
+}
+
+int main(){
+   struct timeval start_time, end_time;
+   long long compute_time;
+   gettimeofday(&start_time,NULL);
+   int r = ackerman(3,6);
+   gettimeofday(&end_time, NULL);
+   compute_time = (end_time.tv_sec - start_time.tv_sec) * 1000000L +
+           (end_time.tv_usec - start_time.tv_usec);
+   printf("Called %d times\n Took %lld microseconds\n overflow: %d underflow: %d \n", count, compute_time, overflow, underflow);
+   return 0;
+}
+
