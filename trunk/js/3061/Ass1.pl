2ac78fc85e53c2b0207161ecedbf7a2c80524667
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-02-13 19:31:40 +0000

    AI. No fukcing clue what so ever

--- trunk/js/3061/Ass1.pl
+++ trunk/js/3061/Ass1.pl
@@ -1,10 +1,10 @@
-%lc([X],KB) :- member([X|L],KB), lcl(L,KB).
-%lc([X|T],KB) :- member([X|L],KB), lcl(L,KB).
+lc([X],KB) :- member([X|L],KB), lcl(L,KB).
+lc([X|T],KB) :- member([X|L],KB), lcl(L,KB).
 
-%lcl([],_).
-%lcl([H|L],KB) :- lc([H],KB), lcl(L,KB).
+lcl([],_).
+lcl([H|L],KB) :- lc([H],KB), lcl(L,KB).
 
 
-%member(X,[X|_]).
-%member(X,[_|L]) :- member(X,L).
+member(X,[X|_]).
+member(X,[_|L]) :- member(X,L).
 
--- trunk/js/3061/assign.pl
+++ trunk/js/3061/assign.pl
@@ -12,4 +12,27 @@ append([],L,L).
 append([H|T],L2,[[H]|L3]) :- append(T,L2,L3). %Append H as a list so it becomes a fact
 
 
-logCon
+bottomUp(Goal,KB):-logCon(KB,C), memberOrig(Goal,C).
+
+
+logCon(KB,C) :- addAll(KB,[],KBNew,C).
+
+addAll(KB,C,KB,C):- empty(KB,C),!.
+addAll(KB,C,KB1,C1):-add(KB,C,KB2,C2), addAll(KB2,C2,KB1,C1).
+
+empty([],_).
+
+memberOrig(X,[X|_]).
+memberOrig(X,[_|T]):-memberOrig(X,T).
+
+add([A|KB],C,KBnew,Cnew):-canBeAdded(A,C),!,A=[B|_]  ,KBnew=KB,Cnew=[[B]|C].
+                          
+add([A|KB],C,KBnew,Cnew):-add(KB,C,KBtemp,Cnew), KBnew=[A|KBtemp].
+
+canBeAdded([_],_).
+canBeAdded([_|T],C):-allMembers(T,C).
+
+
+allMembers([],_).
+allMembers([H|T],X):-member1([H],X),allMembers(T,X).
+
