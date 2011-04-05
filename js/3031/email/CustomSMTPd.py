9eb4c1e823e81283e6717b309d82a1e8b16398aa
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-04-05 16:17:47 +0000

    Gonna try reading the mail in one

--- js/3031/email/CustomSMTPd.py
+++ js/3031/email/CustomSMTPd.py
@@ -3,36 +3,38 @@ import asyncore
 import socket
 from smtpd import SMTPChannel
 import os
-
+import string
+import email
+from email import encoders
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email.MIMEAudio import MIMEAudio
+from email.MIMEImage import MIMEImage
 
 class CustomServer(smtpd.SMTPServer):
 	def process_message(self, sender, sender_addr, rec, data):
 		print "Saving message addressed to ", rec
+		rec = rec[0]
+		
 		mails = "./mail/%s" %rec
+		
 		f = open(mails, 'w')
-		print f
-		print "From ", sender_addr
 		f.write(sender_addr)
-		print "To ", rec
 		f.write(rec[0]) #Assume only one receiver for the moment, this also overwrites any previous mails
-		print "Data ", repr(data)
+		'''
+		msg = email.message_from_string(data)	
+		for part in msg.walk():
+			if part.get_content_type() == "text/plain":
+				e_body = part.get_payload(decode=True)
+		'''		
+		
 		f.write(data)
 		f.close
 		return
 
 
-	def handle_accept(self):
-		conn, addr = self.accept()
-		print "Incomming! %s" % repr(addr)
-		mychannel = MySMTPChannel(self, conn, addr)
 
-class MySMTPChannel(smtpd.SMTPChannel):
-
-	def collect_incomming_data(self, data):
-		print "Collect_incoming_data"
-		self.__line.append(data)
-		print data
-#Extend SMTPChannel and overwrite it's handle read method ( from async_chat). Simple. Not	
 	
 try:
 	server = CustomServer(('127.0.0.1', 1026), None)
