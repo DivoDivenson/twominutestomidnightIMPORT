be0ad271fbb4d5d628577709157c7dcecab20474
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-04-13 12:51:34 +0000

    Qsort parallel code, looks more or less right, don't think it works though

--- trunk/js/3014/exam/qsort.c
+++ trunk/js/3014/exam/qsort.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <omp.h>
 void swap(int arr[],int i, int j)
 {
 	int tmp;
@@ -23,11 +23,19 @@ void qsort(int arr[], int left, int right, int level)
 		if(arr[i] < arr[left])
 			swap(arr, ++last, i);
 	}
-	printf("At level %d \t: ", level);
-	print_array(arr, 12);
+	//printf("At level %d \t: ", level);
+	//print_array(arr, 12);
 	swap(arr, left, last);
-	qsort(arr, left, last -1, level + 1);
-	qsort(arr, last + 1, right, level + 1);
+	#pragma omp parallel sections
+	{
+
+		#pragma omp section
+		qsort(arr, left, last -1, level + 1);
+			
+		#pragma omp section
+		qsort(arr, last + 1, right, level + 1);
+		
+	}
 
 }
 void print_array(int array[], int length)
@@ -42,13 +50,53 @@ void print_array(int array[], int length)
 }
 
 int main(){
-	int array[] = {1,4,6,8,9,2,4,6,4,5,7,2};
-	printf("\t\t  ");
-	print_array(array, 12);
-	qsort(array, 0, 12, 0);
-
+	int array[] = {255,180,4,
+        240,156,4,
+        220,124,4,
+        156,71,4,
+        72,20,4,
+        251,180,4,
+        180,74,4,
+        180,70,4,
+        164,91,4,
+        100,28,4,
+        191,82,4,
+        47,5,4,
+        138,39,4,
+        81,27,4,
+        192,89,4,
+        61,27,4,
+        216,148,4,
+        71,14,4,
+        142,48,4,
+        196,102,4,
+        58,9,4,
+        132,45,4,
+        95,15,4,
+        92,21,4,
+        166,59,4,
+        244,178,4,
+        194,121,4,
+        120,41,4,
+        53,14,4,
+        80,15,4,
+        23,3,4,
+        249,204,4,
+        97,25,4,
+        124,30,4,
+        151,57,4,
+        104,36,4,
+        239,171,4,
+        131,57,4,
+        111,23,4,
+        4,2,4};
+	
+	//int array[] = {1,4,6,8,9,2,4,6,4,5,7,2};
+	print_array(array, 117);
+	qsort(array, 0, 117, 0);
+	printf(" Num threads :%d\n", omp_get_num_threads());
 	printf("Final\n");
-	print_array(array, 12);
+	print_array(array, 117);
 	return 0;
 
 }
