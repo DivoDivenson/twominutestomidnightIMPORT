7287de59625a2e7ae7c7adc5393114a1c7ab460d
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-10-11 08:49:05 +0000

    Haskell done

--- ss/haskell/Ex01.hs
+++ ss/haskell/Ex01.hs
@@ -32,16 +32,39 @@ define d k v = (k,v):d
 --eval d (Divide x y) = eval d x / eval d y
 --eval d (Var i) = fromJust (find d i)
 --eval d (Def i e1 e2) = eval d' e2
---                      where d' = define d i (eval d e1) 
+  --                    where d' = define d i (eval d e1) 
 fromJust (Just a) = a
 
 {- wanted: -}
 eval :: Dictionary Ident Float -> Expression -> Maybe Float
 eval _ (Val x) = Just x
 eval d (Add x y) = addm(eval d x) (eval d y)
-
+eval d (Subtract x y) = subm(eval d x) (eval d y)
+eval d (Multiply x y) = mulm(eval d x) (eval d y)
+eval d (Divide x y) = divm(eval d x) (eval d y)
+eval d (Var i) = (find d i) --find does what we want on its own
+eval d (Def i e1 e2) = if isJust(eval d e1)
+    		       then eval d' e2
+		       else Nothing
+			where d' =  define d i fromJust(eval d e1)
+				   
 
 -- eval d e = Nothing -- need to make this do something useful.
 
 addm (Just a) (Just b) = Just (a + b)
 addm _ _ = Nothing
+
+subm (Just a) (Just b) = Just (a + b)
+subm _ _ = Nothing
+
+mulm (Just a) (Just b) = Just (a + b)
+mulm _ _ = Nothing
+
+divm (Just a) (Just 0) =  Nothing 
+divm (Just a) (Just b) = Just (a / b)
+divm _ _ = Nothing
+
+
+
+
+
