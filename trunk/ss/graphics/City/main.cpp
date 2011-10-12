9f6089989f1e430fe69ec0ff9cfd4d1702928dd0
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-10-12 20:54:29 +0000

    Cube generation code works

--- trunk/ss/graphics/City/main.cpp
+++ trunk/ss/graphics/City/main.cpp
@@ -28,11 +28,113 @@ void handleResize(int w, int h){
 					200.0); //The far z clipping coordinate
 }
 
+//Expects colours + quads to be set
+//Actualy draws a brick (Right rectangular prisim)
+void drawCube(float x, float y, float z, float w, float l, float h){
+	glPushMatrix();
+	glTranslatef(x, y, z);
+
+	glBegin(GL_QUADS);
+
+	//base, check the normal later
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(w, 0.0f, 0.0f);
+    glVertex3f(w, 0.0f, l);
+    glVertex3f(0.0f, 0.0f, l);
+
+
+    //front
+    glNormal3f(0.0f, 0.0f, -1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(w, 0.0f, 0.0f);
+    glVertex3f(w, h, 0.0f);
+    glVertex3f(0.0f, h, 0.0f);
+
+    //back
+    glNormal3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, l);
+    glVertex3f(w, 0.0f, l);
+    glVertex3f(w, h, l);
+    glVertex3f(0.0f, h, l);
+
+    //top
+    glNormal3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, h, 0.0f);
+    glVertex3f(w, h, 0.0f);
+    glVertex3f(w, h, l);
+    glVertex3f(0.0f, h, l);
+
+    //right
+    glNormal3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, l);
+    glVertex3f(0.0f, h, l);
+    glVertex3f(0.0f, h, 0.0f);
+
+    //left
+    glNormal3f(-1.0f, 0.0f, 0.0f);
+    glVertex3f(w, 0.0f, 0.0f);
+    glVertex3f(w, 0.0f, l);
+    glVertex3f(w, h, l);
+    glVertex3f(w, h, 0.0f);
+    
+   
+
+
+	glEnd();
+	glPopMatrix();
+
+
+}
+
+
+float _angle = 30.0f;
 void drawScene(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 
+
+	glTranslatef(0.0f, 0.0f, -15.0f);
+
+	glPushMatrix();
+	//Add ambient light
+	GLfloat ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f};
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
+	//Add positioned light
+	GLfloat lightColor0[] = {0.5f, 0.5f, 0.5f, 1.0f};
+	GLfloat lightPos0[] = {4.0f, 0.0f, 8.0f, 1.0f};//0 for last arg would make it directional
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
+
+	glRotatef(_angle, 1.0f, 0.0f, 0.0f);
+	glColor3f(1.0f, 1.0f, 1.0f);
+   
+   	drawCube(0.0f, 0.0f, 0.0f, 2.0f, 2.0f, 5.0f);
+   	drawCube(4.0f, 0.0f, 4.0f, 2.0f, 2.0f, 5.0f);
+
+
+    glPopMatrix(); //Undo the move to the center of the triangle	
+	glutSwapBuffers(); //Send the 3D scene to the screen
+
+
+
+	
+	
+
+}
+
+void update(int value){
+	_angle += 2.0f;
+	if(_angle > 360){
+		_angle -= 360;
+	}
+
+	glutPostRedisplay();
+
+	glutTimerFunc(25, update, 0);
 }
 
 void initRendering(){
@@ -54,6 +156,7 @@ int main(int argc, char** argv){
 	glutDisplayFunc(drawScene);
 	glutKeyboardFunc(handleKeypress);
 	glutReshapeFunc(handleResize);
+ 	//glutTimerFunc(25, update, 0);
 
 	glutMainLoop();
 		return 0;
