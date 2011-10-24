328777565aaade8d47eb669d2246753b0e4d920e
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-10-24 16:28:13 +0000

    Part 2 done

--- ss/haskell/Tut03.hs
+++ ss/haskell/Tut03.hs
@@ -6,10 +6,12 @@ import Data.Char
 main = do
 	input <- readFile "input.txt"
 	putStr (show (calc input))
-
+--Q1
 calc x = subs ++ [sum subs]
 	where subs =  map calcLine (lines x); 
 
 calcLine::String -> Int
 calcLine x = foldl step 0 (words x)
 	where step acc x = acc + (read x :: Int)
+
+
