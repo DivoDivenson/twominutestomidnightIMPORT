b92e3c4b124bc0ee03244408a0500672383b27d2
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-10-24 21:16:24 +0000

    DistSys lab started

---
+++ ss/distsys/project/server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+import xmlrpclib
+
+class RemoteFile(file):
+
+	def __init__(self,)
+
+	def ls(self):
+		return "Hello"
+
+
+server = SimpleXMLRPCServer(("localhost", 8080))
+print "Serving"
+server.register_instance(RemoteFile())
+server.serve_forever()
+	
