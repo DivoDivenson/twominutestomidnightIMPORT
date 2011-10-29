8a2b0816d7a01f5b25f7378527f0bd233f2de289
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-10-29 20:47:03 +0000

    HMMMMMMMMMMMM

--- ss/distsys/project/client.py
+++ ss/distsys/project/client.py
@@ -1,5 +1,43 @@
 #!/usr/bin/python
 import xmlrpclib
+import os
+from file_id import *
 
-proxy = xmlrpclib.ServerProxy("http://localhost:8080/")
-print proxy.ls()
+
+class ClientProxy():
+
+	server = xmlrpclib.ServerProxy("http://localhost:8080/")
+	open_file = ""
+
+	#Extend for access type for locking
+	def open(self, filename):
+		#first do a lookup, split filename into path and name
+		temp = filename.split('/')
+		name = temp.pop()
+		path = ''
+		for i in temp:
+			path += i + '/'
+
+		#Check if file exists on server
+		response = self.server.lookup(path, name)
+		if response:
+			open_file = file_identifier(response['name'], response['path'])
+			print response.name
+		else:
+			raise IOError ("No such file or directory: " + name)
+
+	def read(self):
+		server.read(open_file)
+		
+
+		
+
+	
+c = ClientProxy()
+c.open('/home/divo/media/sata/College/twominutestomidnight/ss/distsys/project/server.py')
+
+
+
+
+#proxy = xmlrpclib.ServerProxy("http://localhost:8080/")
+#print proxy.ls()
--- ss/distsys/project/server.py
+++ ss/distsys/project/server.py
@@ -1,18 +1,47 @@
 #!/usr/bin/python
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 import xmlrpclib
+import os
+from file_id import *
 
-class RemoteFile(file):
+#Filehandle should be immutable, do this with FileHandle factory? WHAT THE FUCK SHOULD A FILE HANDLE BE?
 
-	def __init__(self):
-		
+
+class RemoteFile():
+
+	
+
+	def __init__(self, filename):
+		self.filename = filename
+
 
 	def ls(self):
 		return "Hello"
 
+class FileServer():
+
+
+	#Put in lots of lovely exception handleing
+	def lookup(self, direc, name):
+		contents = os.listdir(direc)
+		result = None
+		if name in contents:
+			result = file_identifier(name, direc)
+
+		return result
+
+	def read(self, file_id):
+		print file_id
+
+
+
+			
+
+
+
 
 server = SimpleXMLRPCServer(("localhost", 8080))
 print "Serving"
-server.register_instance(RemoteFile())
+server.register_instance(FileServer())
 server.serve_forever()
 	
