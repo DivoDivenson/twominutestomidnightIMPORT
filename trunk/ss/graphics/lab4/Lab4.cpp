5e680a11ae94b64ae72f2004824831bd573b5dec
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-11-08 21:35:51 +0000

    Oh god what have I done

--- trunk/ss/graphics/lab4/Lab4.cpp
+++ trunk/ss/graphics/lab4/Lab4.cpp
@@ -6,7 +6,11 @@
 //#include <gl/glu.h>		// for gluPerspective & gluLookAt
 #include "model3DS.h" // 3DS model support
 
+#define PI 3.141592654f.0
+
 model3DS *teddyModel;
+model3DS * table;
+model3DS * carpark;
 
 void setupScene();
 void updateScene();
@@ -19,11 +23,26 @@ typedef unsigned int DWORD;
 int         tetrahedronAngle=0;
 bool		wireframe=false;
 int         windowId;
+
+int cameraZ;
+int camX, camY;
+
+float xpos = 0, ypos = 0, zpos = 0, xrot = 0, yrot = 0, angle=0.0;
+float lastx, lasty;
+
+
+
 GLuint      textureId;
 DWORD		lastTickCount;
 GLfloat white_light[] = {1.0, 1.0, 1.0, 1.0};
 GLfloat left_light_position[] = {1,0,-1, 1.0}; 
 GLfloat right_light_position[] = {-1,0,-1, 1.0};
+
+void camera (void) {
+    glRotatef(xrot,1.0,0.0,0.0);  
+    glRotatef(yrot,0.0,1.0,0.0);  
+    glTranslated(-xpos,-ypos,-zpos); 
+}
 
 
 void renderScene(){
@@ -40,7 +59,9 @@ void renderScene(){
 
 	// Set view position & direction
 	// (Camera at (0,0,5) looking down the negative Z-axis)
-	gluLookAt(0,0,5,  0,0,-1,  0,1,0);
+	//gluLookAt(0,0,cameraZ,  camX,camY,-1,  0,1,0);
+	//gluLookAt(xpos,ypos,zpos,  xrot + 10,yrot + 10,-1,  0,1,0);
+	camera();
 
 
     // Draw textured tetrahedron
@@ -50,13 +71,16 @@ void renderScene(){
 
     glPushMatrix();
 
-	glTranslatef(0.0,0.0,-1000.0);
+	glTranslatef(0.0,0.0,-600.0);
 	glRotatef(-1*tetrahedronAngle/4.f,0,1,0);
-    glRotatef(-1*tetrahedronAngle/3.f,1,0,0);
-    glRotatef(-1*tetrahedronAngle/2.f,0,0,1);	
+    //glRotatef(-1*tetrahedronAngle/3.f,1,0,0);
+    //glRotatef(-1*tetrahedronAngle/2.f,0,0,1);	
 
 	//DrawTetrahedron();
-	teddyModel->draw();
+	//teddyModel->draw();
+	//table->draw();
+	carpark->draw();
+
 
     glDisable(GL_TEXTURE_2D);
 	glPopMatrix();
@@ -75,7 +99,7 @@ void updateScene(){
 	//lastTickCount=timeGetTime();
     
     // Increment angle for next frame
-    tetrahedronAngle+=2;
+    //tetrahedronAngle+=2;
 
 	// Do any other updates here
 	
@@ -88,18 +112,72 @@ void keypress(unsigned char key, int x, int y){
 	
 	// Test if user pressed ESCAPE (ascii 27)
 	// If so, exit the program
+	if(tetrahedronAngle >= 360){
+		//tetrahedronAngle = 0;
+	}
+
+	printf("%d %d\n", cameraZ, key);
     if(key==27){
 		exitScene();
+	//Forward
 	}
+
+	if(key=='w'){
+	float xrotrad, yrotrad;
+    yrotrad = (yrot / 180 * 3.141592654f);
+    xrotrad = (xrot / 180 * 3.141592654f); 
+    xpos += float(sin(yrotrad));
+    zpos -= float(cos(yrotrad))+10;
+    ypos -= float(sin(xrotrad));
+	//Backward
+	}
+
+	if(key =='s'){
+	float xrotrad, yrotrad;
+    yrotrad = (yrot / 180 * 3.141592654f);
+    xrotrad = (xrot / 180 * 3.141592654f); 
+    xpos -= float(sin(yrotrad));
+    zpos += float(cos(yrotrad))+10;
+    ypos += float(sin(xrotrad));
+	}
+
+	if(key=='d')
+    {
+    float yrotrad;
+	yrotrad = (yrot / 180 * 3.141592654f);
+	xpos += float(cos(yrotrad)) * 0.2 + 10;
+	zpos += float(sin(yrotrad)) * 0.2;
+    }
+
+    if(key=='a')
+    {
+   	float yrotrad;
+	yrotrad = (yrot / 180 * 3.141592654f);
+	xpos -= float(cos(yrotrad)) * 0.2 + 10;
+	zpos -= float(sin(yrotrad)) * 0.2;
+    }
+	
 
 	// Other possible keypresses go here
 	//if(key == 'a'){...}
 }
 
+void mouseMove(int x, int y){
+	int diffx=x-lastx; //check the difference between the current x and the last x position
+	int diffy=y-lasty; //check the difference between the current y and the last y position
+	lastx=x; //set lastx to the current x position
+	lasty=y; //set lasty to the current y position
+	xrot += (float) diffy; //set the xrot to xrot with the addition of the difference in the y position
+	yrot += (float) diffx;// set the xrot to yrot with the addition of the difference in the x position
+}
+
 void setupScene(){
 
 	std::cout<<"Initializing scene..."<<std::endl;
-    
+   
+	cameraZ = -1000;
+	camX =0;
+	camY =0;
 	//Set up Lighting Stuff
 	glLightfv(GL_LIGHT0, GL_POSITION, left_light_position);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);
@@ -115,6 +193,8 @@ void setupScene(){
 
 	//Load the teddy model
 	teddyModel = new model3DS("./teddy/teddy.3ds", 2);
+	table = new model3DS("./table/table.3ds", 50);
+	carpark = new model3DS("./carpark/carpark.3ds", 30);
       
 }
 
@@ -165,6 +245,7 @@ int main(int argc, char *argv[]){
     glutDisplayFunc(renderScene);
     glutIdleFunc(updateScene);
     glutKeyboardFunc(keypress);
+    glutPassiveMotionFunc(mouseMove);
 
     // Setup OpenGL state & scene resources (models, textures etc)
     setupScene();
