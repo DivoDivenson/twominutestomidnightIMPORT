8ef45b64efe1355ce9443f2d0e7d7c8ee28b7fee
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-11-18 23:28:14 +0000

    More work on TGS

--- trunk/ss/distsys/project/client.py
+++ trunk/ss/distsys/project/client.py
@@ -3,43 +3,65 @@ import socket
 import sys
 from crypto import *
 import json
+import time
 
-HOST, PORT = "localhost", 8081
+HOST, AS_PORT = "localhost", 8081
+TGS_PORT = 8082
+
+msg_size = 1024
 
 USER = "divines"
 PASS = "pass"
 
 
-#All of these methods throw socket exceptions
-def login(socket):
-	message = json.dumps({"type" : "login", "user" : USER})
-	sock.send(USER)
-	response = ""
-	while 1:
-		data = sock.recv(1024)
-		if not data: break
-		response += data
+def login():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		sock.connect((HOST, AS_PORT))
+		sock.send(USER)
+		response = ""
+		while 1:
+			data = sock.recv(msg_size)
+			if not data: break
+			response += data
 
-	return response
+		return response
+	
+	finally:
+		sock.close()
+
+def getTicket(as_ticket, tgs_client_key):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		sock.connect((HOST, TGS_PORT))
+		authenticator = json.dumps({"user" : USER, "time" : time.time()}) 
+		authenticator = encrypt(authenticator, tgs_client_key)
+
+		message = json.dumps({"ticket" : as_ticket, "auth" : authenticator})
+		sock.send(message)
+		response = ""
+		while 1:
+			data = sock.recv(msg_size)
+			if not data: break
+			response += data
+	
+		return response
+
+	finally:
+		sock.close()
 
 
 
 
 
 
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-	sock.connect((HOST, PORT))
-	ticket = json.loads(login(sock))
-	key = genKey(PASS)
-	tgs_key = decrypt(ticket['public'], key)
-	print tgs_key
+login_ticket = json.loads(login(), strict=False)
+key = genKey(PASS)
+tgs_client_key = decrypt(login_ticket['public'], key)
+print "Session key obtained, contacting TGS"
+tgs_ticket = getTicket(login_ticket['private'], tgs_client_key)
+print tgs_ticket
 
 	
 
-	
-finally:
-	sock.close()
 
