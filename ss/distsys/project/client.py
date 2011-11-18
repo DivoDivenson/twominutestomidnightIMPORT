1cf44682c93060ed7988101f5be5ed063452dc30
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-11-18 19:57:53 +0000

    Client - TGs login complete

--- ss/distsys/project/client.py
+++ ss/distsys/project/client.py
@@ -1,16 +1,43 @@
 #!/usr/bin/python
 import socket
 import sys
+from crypto import *
 
 HOST, PORT = "localhost", 8081
 
 USER = "divines"
+PASS = "pass"
+
+
+#All of these methods throw socket exceptions
+def login(socket):
+	sock.send("login\n" + USER + '\n')
+	response = ""
+	while 1:
+		data = sock.recv(1024)
+		if not data: break
+		response += data
+
+	return response
+
+
+
+
+
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
 	sock.connect((HOST, PORT))
-	sock.send("login\n" + USER + '\n')
+	ticket = login(sock)
+	key = genKey(PASS)
+	tgs_key = decrypt(ticket.split('\n')[0], key)
+	print tgs_key
+
+	
+
+	
 finally:
 	sock.close()
 
