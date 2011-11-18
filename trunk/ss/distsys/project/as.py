31ba7b36619b769fc808a2e7345e88408f4193e6
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-11-18 19:12:56 +0000

    Encryption working. Working on transactions

--- trunk/ss/distsys/project/as.py
+++ trunk/ss/distsys/project/as.py
@@ -1,16 +1,68 @@
 #!/usr/bin/python
 
 import SocketServer
+import hashlib
+from crypto import *
 
-class AuthenticationServer(SocketServer.StreamRequstHandler):
+#Put all this stuff in files later
+users = {'divines' : "pass"}
+
+#Make these complicated later
+TGS_key = "thisbeakey"
+
+client_TGS_key = "anotherkey"
+
+
+
+
+requests = ["login"]
+
+#Leave expiry out for now
+'''
+Messages
+Client -> Server
+Login request: "login\n<username>\n"
+
+Server -> Client
+Login response: "client_key(<client tgs key>)\nTGS(<username>,<client address>,<client tgs key>\n
+
+
+'''
+
+
+class AuthenticationServer(SocketServer.StreamRequestHandler):
 
 	def handle(self):
-		self.data = self.rfile.readline()
+		self.data = self.rfile.readline().rstrip('\n')
 		print self.data
+		if(self.data == "login"):
+			user = self.rfile.readline().rstrip('\n')
+			client_key = self.lookup(user)
+
+			response = encrypt(client_TGS_key, client_key) + '\n'
+			encrypted_payload = user + '\n' + self.client_address[0] + '\n' + client_TGS_key + '\n'
+			response += encrypt(encrypted_payload, TGS_key)
+			print response
+			
+			
+
+	#Lookup a user an return a client key
+	def lookup(self, name):
+		if name in users:
+			password = users[name]
+			key = hashlib.sha1(password).hexdigest()
+			return key
+
+
 
 
 if __name__ == "__main__":
 	server = SocketServer.TCPServer(("localhost", 8081), AuthenticationServer)
 
-	server.serve_forever()
+	try:
+		print "Authentication Server Running"
+		server.serve_forever()
+	except KeyboardInterrupt:
+		server.shutdown()
+
 
--- trunk/ss/distsys/project/client.py
+++ trunk/ss/distsys/project/client.py
@@ -1,72 +1,16 @@
 #!/usr/bin/python
-import xmlrpclib
-import os
-#from file_id import *
+import socket
+import sys
 
-#Part of the client proxy
-#This class presents the same functionality as a normal file object
-class RemoteFile():
+HOST, PORT = "localhost", 8081
 
-	server = xmlrpclib.ServerProxy("http://localhost:8080/")
-	open_file = ""
-	permission = ""
+USER = "divines"
 
-	def open(self, filename, mode="r"):
-		response = self.server.lookup(filename)
-		if response:
-			self.open_file = response
-			self.permission = mode
-		elif mode == 'r':
-			raise IOError ("No such file or directory: " + filename)
-		elif mode == 'w':
-			response = self.server.create(filename)
-			if(response):
-				self.open_file = response
-				self.permission = mode
-			else:
-				raise IOError ("No such directory: " + filename)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+try:
+	sock.connect((HOST, PORT))
+	sock.send("login\n" + USER + '\n')
+finally:
+	sock.close()
 
-
-
-
-
-
-	def read(self, size=0):
-		result = self.server.read(self.open_file, size)
-
-		return result
-
-	def write(self, string):
-		if(self.permission == 'w'):
-			result = self.server.write(self.open_file, string)
-		else:
-			raise IOError ("File not open for writing: " + self.open_file)
-
-
-	def close(self, string):
-		open_file = ""
-
-		return result
-
-		
-
-
-	
-
-	
-
-#c = RemoteFile()
-#c.open('/home/divo/media/sata/College/twominutestomidnight/ss/distsys/project/server.py')
-#print c.read()
-#b = RemoteFile()
-#b.open('/home/divo/media/sata/College/twominutestomidnight/ss/distsys/project/test.txt', 'w');
-#b.write("This is a test")
-
-
-
-
-
-
-#proxy = xmlrpclib.ServerProxy("http://localhost:8080/")
-#print proxy.ls()
