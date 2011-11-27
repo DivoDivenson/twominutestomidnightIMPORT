f7457d5a678d4e1bb7c4a8772fe967bed86336d6
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-11-27 21:31:04 +0000

    Directory server working

--- ss/distsys/project/client_auth.py
+++ ss/distsys/project/client_auth.py
@@ -11,15 +11,16 @@ HOST, AS_PORT = "localhost", 8081
 TGS_PORT = 8082
 SS_PORT =  8083
 
+servers = ""
 
 USER = "divines"
 PASS = "thisisapassword"
 
-
-def login():
+#Auth with AS
+def login(server):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	try:
-		sock.connect((HOST, AS_PORT))
+		sock.connect(server)
 		sock.send(USER)
 		response = ""
 		while 1:
@@ -32,16 +33,16 @@ def login():
 	finally:
 		sock.close()
 
-
-def getTicket(as_ticket, tgs_client_key, time_stamp):
+#Get a ticket from ticket granting server
+def getTicket(server, as_ticket, tgs_client_key, time_stamp, service):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	try:
-		sock.connect((HOST, TGS_PORT))
+		sock.connect(server)
 
 		authenticator = build_authenticator(time_stamp)
 		authenticator = encrypt(authenticator, tgs_client_key)
 
-		message = json.dumps({"ticket" : as_ticket, "auth" : authenticator})
+		message = json.dumps({"request" : service, "ticket" : as_ticket, "auth" : authenticator})
 		sock.send(message)
 		response = ""
 		while 1:
@@ -55,11 +56,11 @@ def getTicket(as_ticket, tgs_client_key, time_stamp):
 		sock.close()
 
 #Supposed to encrypt tgs_ticket with TGS key. That must be wrong? Encrypted with SS key surly?
-def auth_ss(tgs_ticket, client_ss, time_stamp):
+def auth_ss(server, tgs_ticket, client_ss, time_stamp):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	try:
-		sock.connect((HOST, SS_PORT))
+		sock.connect(server)
 
 		authenticator = build_authenticator(time_stamp)
 		authenticator = encrypt(authenticator, client_ss)
@@ -89,19 +90,31 @@ def auth_ss(tgs_ticket, client_ss, time_stamp):
 def build_authenticator(time_stamp):
 	return json.dumps({"user" : USER, "time" : time_stamp})
 
+#Convert input json to a typle to be used in initing a socket
+def json_to_tuple(json_data, service):
+	return (json_data[service][0], int(json_data[service][1]))
 
 
 #Authenticate and get a Client-SS key to use
-def get_ss_key():
+#Put in error checkign on service type
 
-	login_ticket = json.loads(login(), strict=False)
+#IMPLEMENT THIS
+#def get_ss_key(service_type, user, password):
+def get_ss_key(service_type):
+
+	servers = read_config("./config/servers.json")
+	servers = servers['servers']
+	as_tuple = json_to_tuple(servers, "as")
+
+	login_ticket = json.loads(login(as_tuple), strict=False)
 	key = genKey(PASS)
 	tgs_client_key = decrypt(login_ticket['public'], key)
 	print "Session key obtained, contacting TGS"
 
 	#Convert timestamps to strings for simplicity
 	time_stamp = str(time.time())
-	tgs_response = getTicket(login_ticket['private'], tgs_client_key, time_stamp)
+	tgs_tuple = json_to_tuple(servers, 'tgs')
+	tgs_response = getTicket(tgs_tuple, login_ticket['private'], tgs_client_key, time_stamp, service_type)
 	print "TGS ticket obtained, contacing services server"
 
 	tgs_response = json.loads(tgs_response, strict=False)
@@ -113,7 +126,8 @@ def get_ss_key():
 
 	time_stamp = str(time.time())
 
-	ss_response = auth_ss(tgs_ticket, client_ss, time_stamp)
+	ss_tuple = json_to_tuple(servers, service_type)
+	ss_response = auth_ss(ss_tuple, tgs_ticket, client_ss, time_stamp)
 	ss_response = json.loads(ss_response, strict=False)
 
 	ss_time = ss_response['time']
@@ -125,8 +139,6 @@ def get_ss_key():
 		raise Warning ("Something went wrong contacting SS server")
 
 
-if __name__ == "__main__":
-	print get_ss_key()
 	
 
 
--- ss/distsys/project/dir_server.py
+++ ss/distsys/project/dir_server.py
@@ -5,19 +5,77 @@ import hashlib
 from crypto import *
 import json
 from misc import *
+from ss import *
 
 key = ""
+dir_map = ""
 
 #Should really move these into misc
+
 class TCPServer(SocketServer.TCPServer):
-	allow_reuse_addrss = True
+	allow_reuse_address = True
 
 
-class DirectoryServer(SocketServer.BaseRequestHandler):
+class DirectoryServer(SocketServer.BaseRequestHandler, ServicesServer):
+
+
+	def handle(self):
+		temp = self.request.recv(msg_size)
+		data = temp
+		while len(temp) == msg_size:
+			temp = self.request.recv(msg_size)
+			data += temp
+		
+		data = json.loads(data, strict=False)
+		#Two types of unencrypted packets, login request and requests (the contents of which is encrypted)
+		if(data['type'] == "auth"):
+			self.handle_auth(data, key)
+		#Else, check if user is already authenticated
+		elif(data['type'] == "request"):
+			#If user authenticated
+			print self.users
+			if(self.users.has_key(data['user'])):
+				message = decrypt(data['message'], self.users[data['user']])
+				message = json.loads(message, strict=False)
+
+				filename = message['file']
+				args = message['args']
+
+				#Server only has one function for the moment
+				response = self.map_request(args)
+				self.respond(response, data['user'])
+
+	
+	def map_request(self, filename):
+		print filename
+		result = "not found"
+		for i in dir_map:
+			#Virtual drive
+			temp = dir_map[i][0]
+			if (filename.find(temp) != -1):
+				filename = filename.split(temp)
+				result = dir_map[i][1] + filename[1]
+				break
+
+		return result
+
+
+
+
+
 
 	
 
 if __name__ == "__main__":
-	key = (read_config("./config/fs.json"))['key']
+
+	key = (read_config("./config/ds.json"))['key']
+	dir_map = (read_config("./config/ds.json"))['directories']
 
 	config = (read_config("./config/servers.json"))['servers']['ds']
+	server = TCPServer((config[0], int(config[1])), DirectoryServer)
+
+	try:
+		print "Directory Server Running"
+		server.serve_forever()
+	except KeyboardInterrupt:
+		server.shutdown()
