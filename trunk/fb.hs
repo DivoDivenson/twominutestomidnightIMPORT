a5b37889d1ee9b72f0eff2b9fbcc1ca92ac56e6c
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-12-06 15:23:29 +0000

    some stuffy

---
+++ trunk/fb.hs
@@ -0,0 +1,37 @@
+main = do
+	--strings <- getLine
+	strings
+	let wrds = words strings
+	    len = length $ wrds !! 0
+	putStrLn $ show $ master 0 sent wrds len
+	return ()
+
+
+master :: Int -> [Char] -> [String] -> Int -> Int 
+master i [] _ len= -1 
+master i full@(x:xs) actwrds len
+	| (elem (take len full) actwrds) = do_next full actwrds len i
+	| otherwise = master (i+1) xs actwrds len
+
+do_next :: [Char] -> [String] -> Int -> Int -> Int 
+do_next full@(x:xs) actwrds len i = case res of
+		True -> i
+		False -> master (i+1) xs actwrds len 
+		where res = check (remove_no len full) (remove (take len full) actwrds) len
+
+check :: String -> [String] -> Int -> Bool
+check _ [] _= True 
+check full wrds len 
+	| (elem (wrd) wrds) = check (remove_no len full) nwrds len
+	| otherwise = False
+	where 	wrd = take len full 
+		nwrds = remove wrd wrds
+
+remove el [] = []
+remove el full@(x:xs)
+	| (el == x) = xs
+	| otherwise = x:(remove el xs)
+
+remove_no n  [] = []
+remove_no 0  xs = xs 
+remove_no n full@(x:xs) = remove_no (n-1) xs
