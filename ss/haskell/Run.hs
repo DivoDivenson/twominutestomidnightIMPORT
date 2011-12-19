1aa9c0efb8d2a5449b95cd1a7b618f3547e0f319
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-12-19 22:42:00 +0000

    Very slowly making progress

--- ss/haskell/Run.hs
+++ ss/haskell/Run.hs
@@ -24,16 +24,17 @@ run db (Save, [Filename s]) = do
 
 --run db (Report, [Registrations]) = do
 run db (Distinct, [Column x]) = do
-				--putStrLn $ show $ findColumn x 0 $ head db -- Take first row as column names, seems reasonable
-				putStrLn $ x
+				putStrLn $ show $ findColumn x 0 $ head db -- Take first row as column names, seems reasonable
+				--putStrLn $ x
 				return db
 				
 
 findColumn::String -> Int -> Record -> Int
-findColumn ('$':x) 0 _= read x::Int
+--findColumn ('$':x) 0 _= read x::Int
 findColumn x i ((Value y):ys)
 	| (x == y) = i
 	| otherwise = findColumn x (i+1) ys
+findColumn _ _ [] = -1
 
 
 
@@ -54,9 +55,19 @@ unparseDB db = unlines $ map unparseRecord db
 unparseRecord::Record -> String
 unparseRecord x = joinFields ',' $ map show x	
 
+--Make sure to put the quotes back in
 joinFields::Char -> [String]-> String
 joinFields _ (x:[]) = x
-joinFields seperator (x:xs) = x++[seperator]++joinFields seperator xs
+joinFields seperator (x:xs) = if (hasAnyMine x ',')
+							then ['"']++x++['"']++[seperator]++joinFields seperator xs 
+							else x++[seperator]++joinFields seperator xs
+
+hasAnyMine::String -> Char -> Bool --Cous I can't depend on the libraries apparently
+hasAnyMine [] _ = False
+hasAnyMine (x:xs) c 
+	| (x == c) = True
+	| otherwise = hasAnyMine xs c
+
 
 
 
