7b9559e6df80cfdc1601ee9635039f1fd375e7c4
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-12-19 22:42:00 +0000

    Very slowly making progress

--- trunk/ss/haskell/Parse.hs
+++ trunk/ss/haskell/Parse.hs
@@ -15,7 +15,7 @@ parse2::[String]->Either String (Command,[Args])
 parse2 ("load":x:[]) = Right (Load, [Filename x])
 parse2 ("save":x:[]) = Right (Save, [Filename x])
 parse2 ("report":"registrations":[]) = Right (Report, [Registrations])
-parse2 ("distinct":x:[]) = Right (Distinct, [Column x])
+parse2 ("dist":x:[]) = Right (Distinct, [Column x]) --SHORT HAND FTM
 parse2 (_) = Left "Error"
 
 --String to split, seperator, "tokens"
@@ -23,7 +23,7 @@ split_on::String-> Char -> [String]
 split_on [] _ = [""]
 split_on (x:xs) c 
 	| (x == c) = "":rest
-	| (x == '"') = ([x]++head rest2):(tail rest2)
+	| (x == '"') = rest2--([x]++head rest2):(tail rest2)
 	| otherwise = ([x]++head rest): (tail rest)
 	where 
 		rest = split_on xs c
@@ -33,7 +33,7 @@ split_on (x:xs) c
 split_ignore::String->Char->[String] --as above but ignores sep, only looks for quotes
 split_ignore [] _ = [""]
 split_ignore (x:xs) c
-	| (x == '"') = ([x]++head rest2):(tail rest2)
+	| (x == '"') = rest2--([x]++head rest2):(tail rest2)
 	| otherwise = ([x]++head rest):(tail rest)
 	where 
 		rest = split_ignore xs c
