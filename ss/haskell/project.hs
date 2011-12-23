6f6da71e0cd2893b02d4a5b61ae51e12e9bcc3fd
divoapple <divoapple@c840e694-82d2-11de-a005-139651f65d8c>
2011-12-23 17:20:38 +0000

    Have to submit this

--- ss/haskell/project.hs
+++ ss/haskell/project.hs
@@ -9,26 +9,26 @@ import IO
 --UNIX NEWLINES ONLY
 main::IO()
 main = do
-	execute ([],[],(Just stdout))
+	execute ([],[],(Just stdout), [""])
 --original database and selected part
-execute (_, _, Nothing) =do
+execute (_, _, Nothing, _) =do
 	putStrLn "Daisy Daisy...."
 
-execute (db,sel, file) = do
+execute (db,sel, file, order) = do
 		line <- getLine
 		let ins = parse line
 		case ins of
 			Left str ->do
 							putStrLn str
-							execute (db,sel,file)
+							execute (db,sel,file, order)
 			Right command ->do
-							new <- try(run (db,sel,file) command)
+							new <- try(run (db,sel,file, order) command)
 							--Prevent the user from crashing the program
 							--Yes I do feel slightly bad doing it this way.
 							case new of
 										Left e->do
 												putStrLn $ "Malformed command "++(show e)
-												execute (db, sel, file)
+												execute (db, sel, file, order)
 										Right res->do
 												execute res 
 
